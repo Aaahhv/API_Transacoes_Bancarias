0f886af3f64ef727b38c7d71d012f33e454d35b8
aaahhv <aaahhv2@gmail.com>
2025-03-25 17:06:36 -0300

    adicionei a classe TransacaoModelUtils para facilitar os testes. Arrumei o teste getExtratoClientePorDia_EntradaValida_DeveRetornarExtratoOrdenado

--- transacoes/src/test/java/com/amanda/transacoes/services/relatorios/InformacoesDeClientesServiceTest.java
+++ transacoes/src/test/java/com/amanda/transacoes/services/relatorios/InformacoesDeClientesServiceTest.java
@@ -1,15 +1,12 @@
 package com.amanda.transacoes.services.relatorios;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Year;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +27,7 @@ import com.amanda.transacoes.dtos.relatorios.ClienteEValorDto;
 import com.amanda.transacoes.enums.OperacaoEnum;
 import com.amanda.transacoes.enums.SituacaoOperacaoEnum;
 import com.amanda.transacoes.enums.TipoOperacaoEnum;
+import com.amanda.transacoes.utils.TransacaoModelUtils;
 import com.amanda.transacoes.models.ClienteModel;
 import com.amanda.transacoes.models.TransacaoModel;
 import com.amanda.transacoes.repositories.ClienteRepository;
@@ -55,29 +53,20 @@ public class InformacoesDeClientesServiceTest {
     private List<TransacaoModel> transacoes;
 
     @BeforeEach
-    void setUp() {
+    void setUp() {                                                                                                                                                      
         transacoes = new ArrayList<>();
-        transacoes.add(new TransacaoModel("159001", "159002", 10.0, OperacaoEnum.CREDITO, TipoOperacaoEnum.PIX, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("159001", "159003", 20.0, OperacaoEnum.CREDITO, TipoOperacaoEnum.TED, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("", "159004", 40.0, OperacaoEnum.CREDITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CANCELADO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("159002", null, 80.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.SAQUE, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel(null, "159001", 160, OperacaoEnum.DEBITO, TipoOperacaoEnum.TED, SituacaoOperacaoEnum.PENDENTE, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("XXXXXX", "159001", 320, OperacaoEnum.DEBITO, TipoOperacaoEnum.DOC, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("159003", "", 6400.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("159004", "XXXXXX", 12800.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.add(new TransacaoModel("159004", "159003", 25600.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4")));
-        transacoes.get(0).setDataTransacao(LocalDateTime.of(2024, 9, 15, 12, 31));
-        transacoes.get(1).setDataTransacao(LocalDateTime.of(2024, 4, 15, 12, 32));
-        transacoes.get(2).setDataTransacao(LocalDateTime.of(2024, 5, 15, 12, 33));
-        transacoes.get(3).setDataTransacao(LocalDateTime.of(2024, 6, 15, 12, 34));
-        transacoes.get(4).setDataTransacao(LocalDateTime.of(2024, 7, 15, 12, 35));
-        transacoes.get(5).setDataTransacao(LocalDateTime.of(2024, 8, 15, 12, 36));
-        transacoes.get(6).setDataTransacao(LocalDateTime.of(2024, 9, 15, 12, 37));
-        transacoes.get(7).setDataTransacao(LocalDateTime.of(2024, 9, 15, 12, 38));
-        transacoes.get(8).setDataTransacao(LocalDateTime.of(2024, 8, 15, 12, 38));
+        transacoes.add(TransacaoModelUtils.createTransacao("159001", "159002", 5, OperacaoEnum.CREDITO, TipoOperacaoEnum.PIX, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 4, 15, 12, 31)));
+        transacoes.add(TransacaoModelUtils.createTransacao("159001", "159002", 10.0, OperacaoEnum.CREDITO, TipoOperacaoEnum.PIX, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 9, 15, 12, 31)));
+        transacoes.add(TransacaoModelUtils.createTransacao("159001", "159003", 20.0, OperacaoEnum.CREDITO, TipoOperacaoEnum.TED, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 4, 15, 12, 30)));
+        transacoes.add(TransacaoModelUtils.createTransacao("", "159004", 40.0, OperacaoEnum.CREDITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CANCELADO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 5, 15, 12, 33)));
+        transacoes.add(TransacaoModelUtils.createTransacao("159002", null, 80.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.SAQUE, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 6, 15, 12, 34)));
+        transacoes.add(TransacaoModelUtils.createTransacao(null, "159001", 160, OperacaoEnum.DEBITO, TipoOperacaoEnum.TED, SituacaoOperacaoEnum.PENDENTE, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 4, 15, 12, 35)));
+        transacoes.add(TransacaoModelUtils.createTransacao("XXXXXX", "159001", 320, OperacaoEnum.DEBITO, TipoOperacaoEnum.DOC, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 8, 15, 12, 36)));
+        transacoes.add(TransacaoModelUtils.createTransacao("159003", "", 6400.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 9, 15, 12, 37)));
+        transacoes.add(TransacaoModelUtils.createTransacao("159004", "XXXXXX", 12800.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 9, 15, 12, 38)));
+        transacoes.add(TransacaoModelUtils.createTransacao("159004", "159003", 25600.0, OperacaoEnum.DEBITO, TipoOperacaoEnum.DEPOSITO, SituacaoOperacaoEnum.CONCLUIDO, UUID.fromString("b3f8c1e6-5e3a-4a0b-9b34-3f2c1b37a9e4"), LocalDateTime.of(2024, 8, 15, 12, 38)));
 
-        List<ClienteModel> clientes = new ArrayList<>();
-        
+        List<ClienteModel> clientes = new ArrayList<>();        
         ClienteModel cliente1 = new ClienteModel("Amanda Souza", "591.460.470-26", "159001", true, 1000.0);
         ClienteModel cliente2 = new ClienteModel("Yuri", "619.279.430-86", "159002", true, 1000.0);
         ClienteModel cliente3 = new ClienteModel("Gabriel Ferreira", "970.399.040-12", "159003", true, 1000.0);
@@ -124,7 +113,7 @@ public class InformacoesDeClientesServiceTest {
             if(clienteDto.getCliente().getNumConta().equals("159001")){
                 
             assertEquals(2, clienteDto.getTipoOperacao().get(TipoOperacaoEnum.TED));
-            assertEquals(1, clienteDto.getTipoOperacao().get(TipoOperacaoEnum.PIX));
+            assertEquals(2, clienteDto.getTipoOperacao().get(TipoOperacaoEnum.PIX));
             assertEquals(1, clienteDto.getTipoOperacao().get(TipoOperacaoEnum.DOC));
             }
         }
@@ -135,14 +124,13 @@ public class InformacoesDeClientesServiceTest {
     void getExtratoClienteComFiltro_EntradaValida_DeveRetornarTransacoesFiltradas() {
         Map<String, List<TransacaoModel>> resultado = informacoesDeClienteService.getExtratoClienteComFiltro("159001", OperacaoEnum.CREDITO, TipoOperacaoEnum.PIX);
 
-        assertFalse(resultado.get("159001").isEmpty());
-        assertEquals(1, resultado.get("159001").size());
+        assertEquals(2, resultado.get("159001").size());
     }
 
     
     @Test
     void getClientesCincoMilNoMes_EntradaValida_DeveRetornarClientesComTransacoesAcimaDe5000() {
-        List<TransacaoModel> transacoesNoMes = List.of(transacoes.get(0), transacoes.get(6), transacoes.get(7));
+        List<TransacaoModel> transacoesNoMes = List.of(transacoes.get(1), transacoes.get(7), transacoes.get(8));
         when(transacaoService.findByYearMonthBetween(YearMonth.of(2024,9))).thenReturn(transacoesNoMes);
         Map<YearMonth, List<ClienteEValorDto>> resultado = informacoesDeClienteService.getClientesCincoMilNoMes(YearMonth.of(2024, 9) );
 
@@ -157,9 +145,19 @@ public class InformacoesDeClientesServiceTest {
     void getExtratoClientePorDia_EntradaValida_DeveRetornarExtratoOrdenado() {
         PeriodoDataDto periodo = new PeriodoDataDto(LocalDateTime.of(2024, 4, 15, 12, 30), LocalDateTime.of(2024, 8, 15, 12, 30));
 
-        Map<String, Map<LocalDate, List<TransacaoModel>>> resultado = informacoesDeClienteService.getExtratoClientePorDiaDurantePeriodo("159001", periodo);
+        List<TransacaoModel> transacoesNoPeriodo = List.of(transacoes.get(0), transacoes.get(2), transacoes.get(5));
+        when(transacaoRepository.findByDataTransacaoBetween(LocalDateTime.of(2024, 4, 15, 12, 30), LocalDateTime.of(2024, 8, 15, 12, 30))).thenReturn(transacoesNoPeriodo);
 
-        assertFalse(resultado.isEmpty());
+        Map<String, Map<LocalDate, List<TransacaoModel>>> resultado = informacoesDeClienteService.getExtratoClientePorDiaDurantePeriodo("159001", periodo);
+        List<TransacaoModel> transacoesOrdenadas = resultado.get("159001").get(LocalDate.of(2024, 4, 15));
+
+        for(int i = 0; i < transacoesOrdenadas.size() - 1; i++){
+            LocalDateTime dataAtual = transacoesOrdenadas.get(i).getDataTransacao();
+            LocalDateTime dataSeguinte= transacoesOrdenadas.get(i + 1).getDataTransacao();
+
+            assertTrue(dataAtual.isBefore(dataSeguinte) || dataAtual.equals(dataSeguinte));
+        }
+
         assertTrue(resultado.containsKey("159001"));
     }
 }
