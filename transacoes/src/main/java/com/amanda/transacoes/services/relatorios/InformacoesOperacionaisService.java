745c15343a21b5c6b9ccd8103d984b1e7243f39c
aaahhv <aaahhv2@gmail.com>
2025-03-25 13:34:23 -0300

    reduzindo duplicidade de codigo

--- transacoes/src/main/java/com/amanda/transacoes/services/relatorios/InformacoesOperacionaisService.java
+++ transacoes/src/main/java/com/amanda/transacoes/services/relatorios/InformacoesOperacionaisService.java
@@ -3,15 +3,15 @@ package com.amanda.transacoes.services.relatorios;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amanda.transacoes.dtos.relatorios.RelatorioOperacionalDto;
-import com.amanda.transacoes.enums.OperacaoEnum;
 import com.amanda.transacoes.enums.SituacaoOperacaoEnum;
-import com.amanda.transacoes.enums.TipoOperacaoEnum;
 import com.amanda.transacoes.models.TransacaoModel;
 import com.amanda.transacoes.services.TransacaoService;
 
@@ -20,58 +20,33 @@ public class InformacoesOperacionaisService {
     @Autowired
     private TransacaoService transacaoService;
 
-    public RelatorioOperacionalDto getOperacoes(YearMonth mes) {
-        List<TransacaoModel> transacoes = transacaoService.findByYearMonthBetween(mes);
-    
-        Map<OperacaoEnum, RelatorioOperacionalDto.MovimentacaoDto> movimentacoes = transacoes.stream()
-            .filter(transacao -> transacao.getSituacao().equals(SituacaoOperacaoEnum.CONCLUIDO))
-            .collect(Collectors.groupingBy(
-                TransacaoModel::getOperacao,
-                Collectors.collectingAndThen(
-                    Collectors.summarizingDouble(TransacaoModel::getValor),
-                    stats -> new RelatorioOperacionalDto.MovimentacaoDto((int) stats.getCount(), stats.getSum())
-                )
-            ));
-    
-        RelatorioOperacionalDto relatorio = new RelatorioOperacionalDto("operacao");
-        movimentacoes.forEach((tipo, movimentacao) -> relatorio.adicionarMovimentacao(tipo.name(), movimentacao));
-    
-        return relatorio;
+    public RelatorioOperacionalDto getOperacoes(YearMonth mes) {   
+        return getRelatorio(mes, transacao -> transacao.getSituacao().equals(SituacaoOperacaoEnum.CONCLUIDO), TransacaoModel::getOperacao, "operacao");
     }
 
     public RelatorioOperacionalDto getTipoOperacoes(YearMonth mes) {
-        List<TransacaoModel> transacoes = transacaoService.findByYearMonthBetween(mes);
-
-        Map<TipoOperacaoEnum, RelatorioOperacionalDto.MovimentacaoDto> movimentacoes = transacoes.stream()
-            .filter(transacao -> transacao.getSituacao().equals(SituacaoOperacaoEnum.CONCLUIDO))
-            .collect(Collectors.groupingBy(
-                TransacaoModel::getTipoOperacao,
-                Collectors.collectingAndThen(
-                    Collectors.summarizingDouble(TransacaoModel::getValor),
-                    stats -> new RelatorioOperacionalDto.MovimentacaoDto((int) stats.getCount(), stats.getSum())
-                )
-            ));
-
-        RelatorioOperacionalDto relatorio = new RelatorioOperacionalDto("tipo_operacao");
-        movimentacoes.forEach((tipo, movimentacao) -> relatorio.adicionarMovimentacao(tipo.name(), movimentacao));
-
-        return relatorio;
+        return getRelatorio(mes, transacao -> transacao.getSituacao().equals(SituacaoOperacaoEnum.CONCLUIDO), TransacaoModel::getTipoOperacao, "tipo_operacao");
     }
 
     public RelatorioOperacionalDto getSituacaoOperacao(YearMonth mes) {
+        return getRelatorio(mes, transacao -> true, TransacaoModel::getSituacao, "situacao_operacao");
+    }
+
+    public <T> RelatorioOperacionalDto getRelatorio(YearMonth mes, Predicate<TransacaoModel> filtro, Function<TransacaoModel, T> agrupador, String nomeRelatorio) {
         List<TransacaoModel> transacoes = transacaoService.findByYearMonthBetween(mes);
 
-        Map<SituacaoOperacaoEnum, RelatorioOperacionalDto.MovimentacaoDto> movimentacoes = transacoes.stream()
+        Map<T, RelatorioOperacionalDto.MovimentacaoDto> movimentacoes = transacoes.stream()
+            .filter(filtro)
             .collect(Collectors.groupingBy(
-                TransacaoModel::getSituacao,
+                agrupador,
                 Collectors.collectingAndThen(
                     Collectors.summarizingDouble(TransacaoModel::getValor),
                     stats -> new RelatorioOperacionalDto.MovimentacaoDto((int) stats.getCount(), stats.getSum())
                 )
             ));
 
-        RelatorioOperacionalDto relatorio = new RelatorioOperacionalDto("situacao_operacao");
-        movimentacoes.forEach((tipo, movimentacao) -> relatorio.adicionarMovimentacao(tipo.name(), movimentacao));
+        RelatorioOperacionalDto relatorio = new RelatorioOperacionalDto(nomeRelatorio);
+        movimentacoes.forEach((tipo, movimentacao) -> relatorio.adicionarMovimentacao(tipo.toString(), movimentacao));
 
         return relatorio;
     }
